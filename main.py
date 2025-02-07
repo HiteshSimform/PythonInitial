8474cd154519676048828349f92a349900fd5073
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-02-07 09:55:32 +0530

    String, len

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("1")
+print("Hello World!!")
+
+message = "Hello"
+print(message)
+
+message1 = 'Python\'s Learning'  # \ use for single quort during string
+print(message1)
+
+# Multiline String
+
+message2 = """Hitesh Jethava
+Simform Solutions LLP"""
+print(message2)
+
+# len() use for find the length of the string
+print(len(message2))
+print(message2[0])
+print(message2[-1])
+# print(message2[50])
+print(message2[0:10])
